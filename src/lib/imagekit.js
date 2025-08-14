26bba929c7ad6f6e97406f31e96faa3764e9c7da
iftikhar ali <ifti.hazara205@gmail.com>
2025-08-14 17:12:32 +0500

    papajonson11

--- src/lib/imagekit.js
+++
@@ -1,9 +0,0 @@
-import ImageKit from "imagekit";
-
-const imageKitConfig = new ImageKit({
-       publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY,
-    privateKey: process.env.IMAGEKIT_PRIVATE_KEY,
-    urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT
-});
-
-export default imageKitConfig;
