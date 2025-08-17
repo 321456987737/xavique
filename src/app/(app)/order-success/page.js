5904aad9dac3121a2c544452525e3eb796a8de0d
iftikhar ali <ifti.hazara205@gmail.com>
2025-08-17 20:27:54 +0500

    pop6

--- src/app/(app)/order-success/page.js
+++ src/app/(app)/order-success/page.js
@@ -42,7 +42,7 @@ export default function OrderSuccessPage() {
 
   return (
     <div className="min-h-screen pt-[120px] bg-[#0A0A0A] py-12 px-4 sm:px-6 lg:px-8">
-      <div className="max-w-3xl mx-auto">
+      <div className="max-w-3xl flex flex-col justify-center items-center mx-auto">
 
         <div className="flex justify-center mb-8">
           <div className="flex items-center">
@@ -91,7 +91,7 @@ export default function OrderSuccessPage() {
             </div>
           )}
           
-          <h1 className="text-3xl font-bold text-white mb-4">
+          <h1 className="text-3xl font-bold text-white mb-4 mx-auto ">
             Order Confirmed!
           </h1>
           
