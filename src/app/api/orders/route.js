9c72caff853dd9b75c01821d5dbfb21539a4162f
iftikhar ali <ifti.hazara205@gmail.com>
2025-08-17 18:41:12 +0500

    pop5

--- src/app/api/orders/route.js
+++ src/app/api/orders/route.js
@@ -2,25 +2,23 @@
 import Order from '@/model/Order';
 import { NextResponse } from 'next/server';
 import { dbConnect } from '@/lib/db';
+
 export async function POST(request) {
   try {
-     await dbConnect()
-     console.log(1)
+    await dbConnect();
+
     const { customer, items, total, paymentMethod, stripeSessionId } = await request.json();
-     console.log(1)
-     console.log("customer",customer,"items",items,"total",total,"paymentMethod",paymentMethod,"stripeSessionId",stripeSessionId)
+
+    console.log("customer", customer, "items", items, "total", total, "paymentMethod", paymentMethod, "stripeSessionId", stripeSessionId);
 
     const order = await Order.create({
-      data: {
-        customer: JSON.stringify(customer),
-        items: JSON.stringify(items),
-        total: total,
-        paymentMethod: paymentMethod,
-        stripeSessionId: stripeSessionId || null,
-        paymentStatus: paymentMethod === 'cash' ? 'pending' : 'paid',
-      }
+      customer: customer,           // if schema is Mixed, no need to JSON.stringify
+      items: items,
+      total: total,
+      paymentMethod: paymentMethod,
+      stripeSessionId: stripeSessionId || null,
+      paymentStatus: paymentMethod === 'cash' ? 'pending' : 'paid',
     });
-     console.log(1)
 
     return NextResponse.json(order, { status: 201 });
   } catch (error) {
